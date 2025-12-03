ccf4efb00916537b3b0274c2f76635bac0faacb2
jashwanthgadiraju2007-gif <jashwanthgadiraju2007@gmail.com>
2025-12-03 19:28:28 +0530

    Add password generator function and main execution

--- password_generator/main.py
+++ password_generator/main.py
@@ -1,1 +1,10 @@
-
+import random
+import string
+def generate_password(length):
+    characters=string.ascii_letters+string.punctuation+string.digits
+    password="".join(random.choice(characters) for _ in range(length))
+    return password
+def main():
+ length=int(input("enter a length:"))
+ print("generated password : "+generate_password(length))
+main()
